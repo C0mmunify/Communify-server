090f10602382a5c25fce1083dc1787bfc0ccf06a
franklyn <asafoadjeif@gmail.com>
2023-01-26 19:30:39 +0200

    user route created with index model

---
+++ db/migrations/2_seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO council (id, area)
+VALUES
+    (1, 'Sheffield'),
+    (2, 'Cardiff'),
+    (3, 'London');
+
+INSERT INTO users (
+    id,
+    name, 
+    username, 
+    email,
+    phone,
+    age,
+    council_id,
+    admin
+) 
+VALUES
+    (1, 'Noah Platton', 'nplatton' ,'noah.platton@getfutureproof.co.uk', 07964170706, 24, 1, True),
+    (2, 'Tom Grainger', 'Graingertom', 'thomas.grainger@getfutureproof.co.uk', null, 24, 2, True),
+    (3, 'Franklyn Asafo', 'asafoadjeif', 'franjly.asafo@getfutureproof.co.uk', null, 24, 3, True);
+
+INSERT INTO events (
+    id, 
+    title, 
+    description,
+    location,
+    council_id,
+    creator_id,
+    image,
+    spaces,
+    date_created,
+    date_occurring
+) 
+VALUES
+    (1, 'Example Event', 'This is an example event.', 'Noahs house', 1, 1, 'some image', 3, TIMESTAMP("2023-01-10",  "13:10:00"), TIMESTAMP("2023-01-23",  "18:00:00"));
---
+++ server/Models/userModel.js
@@ -0,0 +1,26 @@
+
+const db = require('../dbConfig')
+class User {
+    constructor(data){
+        this.id = data.id
+        this.name = data.name
+        this.email = data.email
+        this.phone = data.phone
+        this.age = data.age
+        this.address = data.address
+        this.council = data.council
+        this.admin = data.admin
+    }
+    static findById (id) {
+        return new Promise (async (resolve, reject) => {
+            try {
+                let userData = await db.query(`SELECT * FROM users WHERE id = $1;`, [ id ]); 
+                let user = new User(userData.rows[0]);
+                resolve (user);
+            } catch (err) {
+                reject('User not found');
+            }
+        });
+    }
+}
+module.exports = User;
