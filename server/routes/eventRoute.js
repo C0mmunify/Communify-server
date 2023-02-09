2c205e7d1481252d59bdb23a702b1fce4323a892
Noah Platton <noahplatton@gmail.com>
2023-02-09 16:53:00 +0000

    [NP] Fix server

--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -1,15 +1,16 @@
-const express = require('Express');
+const express = require("Express");
 const router = express.Router();
-const path = require('path');
-const eventControllers = require('../controllers/eventControllers')
+const path = require("path");
+const eventControllers = require("../controllers/eventControllers");
 
-const { verifyToken } = require("../middleware/jwtAuth"); 
+const { verifyToken } = require("../middleware/jwtAuth");
 
 router.get("/:event_name", eventControllers.findByTitle);
-router.get("/:user_name)", eventControllers.findByAttendeeName);
-router.get("/:event_id")
+router.get("/:user_name", eventControllers.findByAttendeeName);
+router.get("/:event_id");
 router.get("/:area", eventControllers.findByArea);
 router.post("/", eventControllers.createEvent);
 router.patch("/:event_id", eventControllers.updateEvent);
 router.delete("/:event_id", eventControllers.deleteEvent);
-modeule.exports = router;
+
+module.exports = router;
