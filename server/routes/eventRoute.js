46cc6aa67fd2eaa798dcd2a46565adeee9c74418
Noah Platton <noahplatton@gmail.com>
2023-02-11 00:47:57 +0000

    [NP] Fix server

--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -1,14 +1,14 @@
 const express = require("Express");
 const router = express.Router();
-const path = require("path");
 const eventControllers = require("../controllers/eventControllers");
 
 const { verifyToken } = require("../middleware/jwtAuth");
 
+router.get("/", eventControllers.findAllEvents);
+router.get("/:event_id", eventControllers.findById);
 router.get("/:event_name", eventControllers.findByTitle);
 router.get("/:user_name", eventControllers.findByAttendeeName);
-router.get("/:event_id");
-router.get("/:area", eventControllers.findByArea);
+// router.get("/:area", eventControllers.findByArea);
 router.post("/", eventControllers.createEvent);
 router.patch("/:event_id", eventControllers.updateEvent);
 router.delete("/:event_id", eventControllers.deleteEvent);
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,11 +4,12 @@ const { verifyToken } = require("../middleware/jwtAuth");
 
 const router = express.Router();
 
-router.get("/", verifyToken, userControllers.getUsers);
+router.get("/", verifyToken, userControllers.findAllUsers);
 router.get("/:user_id", verifyToken, userControllers.findById);
 // router.patch("/:user_id")
 router.get("/:user_name", verifyToken, userControllers.findByName);
-router.patch("/:user_id", verifyToken,  userControllers.updateUser);
+router.patch("/:user_id", verifyToken, userControllers.updateUser);
 router.patch("/:user_id/password", verifyToken, userControllers.updatePass);
 router.delete("/:user_id", verifyToken, userControllers.deleteUser);
+
 module.exports = router;
