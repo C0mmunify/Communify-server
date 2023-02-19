18eac6797dcc91755763de3d081e1774f2e64eee
Noah Platton <noahplatton@gmail.com>
2023-02-19 13:00:26 +0000

    [NP] sync branch with dev

--- server/tests/integration/resetSeeds.sql
+++ server/tests/integration/resetSeeds.sql
@@ -6,17 +6,18 @@ INSERT INTO users (
     phone,
     age,
     council,
+    profile_image,
     admin
 ) 
 VALUES
-    ('Test User 1', 'testuser1@email.com', '1234567890', 14, "council 1", false),
-    ('Test User 2', 'testuser2@email.com', '2345678901', 12, "council 2", false),
-    ('Test User 3', 'testuser3@email.com', '3456789012', 14, "council 3", false),
-    ('Test User 4', 'testuser4@email.com', '4567890123', 17, "council 1", false),
-    ('Test User 5', 'testuser5@email.com', '5678901234', 10, "council 4", false),
-    ('Test User 6', 'testuser6@email.com', '6789012345', 11, "council 1", false),
-    ('Test User 7', 'testuser7@email.com', '7890123456', 10, "council 3", false),
-    ('Test User 8', 'testuser8@email.com', '8901234567', 15, "council 3", false);
+    ('Test User 1', 'testuser1@email.com', '1234567890', 14, 'council 1', 'image 1', false),
+    ('Test User 2', 'testuser2@email.com', '2345678901', 12, 'council 2', 'image 2', false),
+    ('Test User 3', 'testuser3@email.com', '3456789012', 14, 'council 3', 'image 3', false),
+    ('Test User 4', 'testuser4@email.com', '4567890123', 17, 'council 1', 'image 4', false),
+    ('Test User 5', 'testuser5@email.com', '5678901234', 10, 'council 4', 'image 5', false),
+    ('Test User 6', 'testuser6@email.com', '6789012345', 11, 'council 1', 'image 6', false),
+    ('Test User 7', 'testuser7@email.com', '7890123456', 10, 'council 3', 'image 7', false),
+    ('Test User 8', 'testuser8@email.com', '8901234567', 15, 'council 3', 'image 8', false);
 
 INSERT INTO events (
     title, 
@@ -24,16 +25,17 @@ INSERT INTO events (
     location,
     council,
     creator_id,
+    image,
     spaces_total,
     spaces_remaining,
     date_created,
     date_occurring,
 ) 
 VALUES
-    ('Example Event 1', 'This is an example event 1.', 'Address 1', "council 1", 1, 3, 1, '2023-01-23 00:00:00', '2023-01-28 12:00:00'),
-    ('Example Event 2', 'This is an example event 2.', 'Address 2', "council 2", 2, 5, 5, '2023-01-23 00:00:00', '2023-01-28 12:00:00'),
-    ('Example Event 3', 'This is an example event 3.', 'Address 3', "council 3", 3, 3, 0, '2023-01-23 00:00:00', '2023-01-28 12:00:00'),
-    ('Example Event 4', 'This is an example event 4.', 'Address 4', "council 1", 1, 4, 1, '2023-01-23 00:00:00', '2023-01-28 12:00:00');
+    ('Example Event 1', 'This is an example event 1.', 'Address 1', 'council 1', 1, 'image 1', 3, 1, '2023-01-23 00:00:00', '2023-01-28 12:00:00'),
+    ('Example Event 2', 'This is an example event 2.', 'Address 2', 'council 2', 2, 'image 2', 5, 5, '2023-01-23 00:00:00', '2023-01-28 12:00:00'),
+    ('Example Event 3', 'This is an example event 3.', 'Address 3', 'council 3', 3, 'image 3', 3, 0, '2023-01-23 00:00:00', '2023-01-28 12:00:00'),
+    ('Example Event 4', 'This is an example event 4.', 'Address 4', 'council 1', 1, 'image 4', 4, 1, '2023-01-23 00:00:00', '2023-01-28 12:00:00');
 
 INSERT INTO attendees (
     user_id,
@@ -54,11 +56,11 @@ INSERT INTO auth (
     password_digest
 )
 VALUES
-    ('1', '$2y$10$HNjSrS1kPZkiX3hA9K6QSOO2PdBb8DTqhFHs/opy8eH3eOEdZiooG'),
-    ('2', '$2y$10$55J4Ma28F6W4rQgbVwBh9ugovgzmGhNcsKrotIk6XOF2qwmZQJz.O'),
-    ('3', '$2y$10$ZwI10iOMnWjIwg9RcoJyKeBo.cASJSS0RGsdYNTXM4112HAG6g.cS'),
-    ('4', '$2y$10$vQrI8YHdDNNFUM5AeIfpz.H6XcZK.YvmNIE/ouIMXivQf1qRPrtgu'),
-    ('5', '$2y$10$QYBV9KzLizutj5Xp5tTIIuLw6JAZcsmM2Ce5lFpIfJEamzOwISfc2'),
-    ('6', '$2y$10$IGDlNBfOIBNtTx5G9vt6vOUDAENQPDgjGbfwy.0bQ8jq3FTbYyRT6'),
-    ('7', '$2y$10$.JtUEbXMADgVmi6ojNy/NeC5QIWCGg6ah/Qlb4He/jozYIOwQUocC'),
-    ('8', '$2y$10$4kS0CknOUuBHVjSrvwK4r.YYJvb5bw3xDkrjMNCKeEkcK6KzmnJCq');
+    ('1', '$2y$10$AcA1dC5aWuK9/RdQ/9/1I.9JU5tdQMAyieOHkSLKMnwhmyehKmIbq'),
+    ('2', '$2y$10$LFY9s2O6ipYwLyG1tlkWAe1MS5VIdR1qAEPSmvmVISmyL9PDG5U62'),
+    ('3', '$2y$10$5ZMr424hhUyNKqSKx8eEzemwbCH3M1KEaipqlQmagHVg6PkYSStBq'),
+    ('4', '$2y$10$cAWYgHh3Yt722cXDNKM/z.8SSf7ILUgL0xSy1KFTzkBzE97FAkB0S'),
+    ('5', '$2y$10$jdiUvxpMn1WMxFKRCL/nxOMTnih6hyczEaCtDPGzZlp3SK4TKBjVa'),
+    ('6', '$2y$10$GPSzhhhJ1Tsv5qFJLzsCgO3nB1BdaNDf48Icy32iJC252/qx2Zjby'),
+    ('7', '$2y$10$pyXXkz73Fu1Vl3L/.cNR6OTI7Ez4dHiLwLZcSfO.OGr9heDtnG72C'),
+    ('8', '$2y$10$O3eVaDCdLs2vxFDmkxHhmeF7N8T2legVXwdtjUI2sMM.SFhm/bYj6');
--- server/tests/integration/userEndpoints.spec.js
+++ server/tests/integration/userEndpoints.spec.js
@@ -1,23 +1,189 @@
-describe("User endpoints", () => {
+describe("successful endpoints", () => {
     let api;
+    let token;
+
+    beforeAll(async () => {
+        api = app.listen(port, () =>
+            console.log("Test server running on port 5000...")
+        );
+        await resetTestDB();
+        const loginResponse = await request(api)
+            .post("/auth/login")
+            .send({ email: "testuser1@email.com", password: "password1" })
+            .set("Accept", "application/json");
+        token = loginResponse._body.Bearer;
+    }, 100000);
+
     beforeEach(async () => {
+        console.log("-----------------------------------");
         await resetTestDB();
     });
 
-    beforeAll(async () => {
-        api = app.listen(5000, () =>
-            console.log("Test server running on port 5000")
-        );
-    });
-
     afterAll(async () => {
-        console.log("Gracefully stopping test server");
-        await api.close();
+        console.log("Stopping test server");
+        await api.close((err) => {});
     });
 
-    it("should retrieve a user based on id", async () => {
-        const res = await request(api).get("/user/1");
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.name).toEqual("Example User 1");
+    describe("user", () => {
+        describe("authenticated GET /", () => {
+            test("it should return all users", async () => {
+                console.log(token);
+                const res = await request(api)
+                    .get("/user")
+                    .set("authorization", token)
+                    .set("Accept", "application/json");
+                expect(res.statusCode).toEqual(200);
+                expect(res.body).toHaveLength(8);
+            });
+        });
+
+        describe("unauthenticated GET /", () => {
+            test("it should return 403 in case of invalid token", async () => {
+                const res = await request(api)
+                    .get("/user")
+                    .set("authorization", "Bearer VERY.GOOD.REAL.TOKEN");
+                expect(res.statusCode).toEqual(403);
+            });
+        });
+
+        describe("authenticated GET /:user_id", () => {
+            test("it should retrieve user based on ID", async () => {
+                const res = await request(api)
+                    .get("/user/2")
+                    .set("authorization", token)
+                    .set("Accept", "application/json");
+                expect(res.statusCode).toEqual(200);
+                expect(res.body.email).toEqual("testuser2@email.com");
+            });
+        });
+
+        describe("unauthenticated GET /:user_id", () => {
+            test("it should return 403 if invalid token sent", async () => {
+                const res = await request(api)
+                    .get("/user/2")
+                    .set("authorization", "Bearer VERY.GOOD.REAL.TOKEN");
+                expect(res.statusCode).toEqual(403);
+            });
+        });
+
+        describe("authenticated GET /user_name/:user_name", () => {
+            test("it should retrieve user based on user's name", async () => {
+                const res = await request(api)
+                    .get("/user/user_name/Test%20User%203")
+                    .set("authorization", token)
+                    .set("Accept", "application/json");
+                expect(res.statusCode).toEqual(200);
+                expect(res.body.email).toEqual("testuser3@email.com");
+            });
+        });
+
+        describe("unauthenticated GET /user_name/:user_name", () => {
+            test("it should return 403 if invalid token sent", async () => {
+                const res = await request(api)
+                    .get("/user/3")
+                    .set("authorization", "Bearer VERY.GOOD.REAL.TOKEN");
+                expect(res.statusCode).toEqual(403);
+            });
+        });
+
+        describe("authenticated PATCH /:user_id", () => {
+            test("it should update user data", async () => {
+                const res = await request(api)
+                    .patch("/user/7")
+                    .send(
+                        JSON.stringify({
+                            name: "Test User 7",
+                            email: "newemail@email.com",
+                            phone: "7890123456",
+                            age: 14,
+                            council: "council 3",
+                            profile_image: "image 7",
+                        })
+                    )
+                    .set("Content-Type", "application/json");
+                expect(res.statusCode).toEqual(200);
+                expect(res.body.id).toBe(7);
+                expect(res.body.email).toBe("newemail@email.com");
+            });
+        });
+
+        describe("unauthenticated PATCH /:user_id", () => {
+            test("it should return 403 if invalid token sent", async () => {
+                const res = await request(api)
+                    .patch("/user/7")
+                    .send(
+                        JSON.stringify({
+                            name: "Test User 7",
+                            email: "newemail@email.com",
+                            phone: "7890123456",
+                            age: 14,
+                            council: "council 3",
+                            profile_image: "image 7",
+                        })
+                    )
+                    .set("authorization", "Bearer VERY.GOOD.REAL.TOKEN");
+                expect(res.statusCode).toEqual(403);
+            });
+        });
+
+        describe("authenticated PATCH /:user_id/password", () => {
+            test("it should update user data", async () => {
+                const res = await request(api)
+                    .patch("/user/7/password")
+                    .send(
+                        JSON.stringify({
+                            password: "newpassword",
+                        })
+                    )
+                    .set("Content-Type", "application/json");
+                expect(res.statusCode).toEqual(200);
+                expect(res.body.id).toBe(7);
+                expect(res.body.email).toBe("newemail@email.com");
+            });
+        });
+
+        describe("unauthenticated PATCH /:user_id/password", () => {
+            test("it should return 403 if invalid token sent", async () => {
+                const res = await request(api)
+                    .patch("/user/7/password")
+                    .send(
+                        JSON.stringify({
+                            password: "newpassword",
+                        })
+                    )
+                    .set("authorization", "Bearer VERY.GOOD.REAL.TOKEN");
+                expect(res.statusCode).toEqual(403);
+            });
+        });
+
+        describe("authenticated DELETE /:user_id", () => {
+            test("it should update user data", async () => {
+                const res1 = await request(api)
+                    .get("/user")
+                    .set("authorization", token)
+                    .set("Accept", "application/json");
+                expect(res1.statusCode).toEqual(200);
+                expect(res1.body).toHaveLength(8);
+                const res2 = await request(api)
+                    .patch("/user/7")
+                    .set("Content-Type", "application/json");
+                expect(res2.statusCode).toEqual(200);
+                const res3 = await request(api)
+                    .get("/user")
+                    .set("authorization", token)
+                    .set("Accept", "application/json");
+                expect(res3.statusCode).toEqual(200);
+                expect(res3.body).toHaveLength(7);
+            });
+        });
+
+        describe("unauthenticated DELETE /:user_id", () => {
+            test("it should return 403 if invalid token sent", async () => {
+                const res = await request(api)
+                    .patch("/user/7")
+                    .set("authorization", "Bearer VERY.GOOD.REAL.TOKEN");
+                expect(res.statusCode).toEqual(403);
+            });
+        });
     });
 });
