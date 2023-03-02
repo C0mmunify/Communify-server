836c257aa5dfde8f4518b8ad2e83f9be66d4c7c3
Noah Platton <noahplatton@gmail.com>
2023-03-02 18:27:40 +0000

    [NP] Update test config and add ENVIRONMENT variable

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,11 +1,29 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    connectionString: process.env.DB_CONNECTION_STRING,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+let pool;
+switch (process.env.ENVIRONMENT) {
+    case "prod":
+        pool = new Pool({
+            connectionString: process.env.DB_CONNECTION_STRING,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        });
+        break;
+    case "dev":
+        pool = new Pool({
+            connectionString: process.env.DEV_DB_CONNECTION_STRING,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        });
+        break;
+    case "test":
+        pool = new Pool();
+        break;
+    default:
+        console.log("Please set ENVIRONMENT variable [test,dev,prod].");
+}
 
 module.exports = pool;
