42dfcc775b6e8c27889af831ae1a3490396b07c6
Noah Platton <noahplatton@gmail.com>
2023-03-02 18:26:33 +0000

    [NP] Add Windows support to test scripts

--- _scripts/coverage.sh
+++ _scripts/coverage.sh
@@ -1,2 +1,2 @@
 docker compose -f docker-compose.yaml -f docker-compose.test.yaml up -d
-docker exec -it c0mmunify_test_api bash -c "npm install && npm run coverage"
+winpty docker exec -it c0mmunify_test_api bash -c "npm install && npm run coverage"
